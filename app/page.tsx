37766489dfee89e4572cb47d3d07ecc9a91233e6
bluecode-hub <mahekg819@gmail.com>
2026-01-28 12:19:18 +0530

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Prism from "./components/PrismBackground"; // or Prism if you rename it
+import Prism from "./components/PrismBackground";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,12 +12,21 @@ export default function Home() {
           zIndex: 1,
           height: "100vh",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
           color: "white",
+          gap: "20px",
         }}
       >
-        <h1>MES 2026</h1>
+        <h1 className="text-5xl font-bold">MES 2026</h1>
+
+        <Link
+          href="/signup"
+          className="px-6 py-3 bg-[#E91E63] text-white rounded-lg font-semibold hover:bg-[#D81B60]"
+        >
+          Sign Up
+        </Link>
       </div>
     </main>
   );
---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import Signup from "../components/signup";
+
+export default function SignupPage() {
+  return (
+    <main style={{ position: "relative", minHeight: "100vh" }}>
+      
+
+        <Signup />
+      
+    </main>
+  );
+}
